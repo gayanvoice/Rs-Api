6b77e83cbdf9f5890e5d4241556b1c6d7e442e09
Gayan Kuruppu <gayan.kuruppu@live.com>
2023-03-02 18:14:03 +0000

    Add Status

---
+++ Rs-Api/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Rs_Api.Models;
+
+namespace RsApi.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+        }
+
+        [HttpGet("Status")]
+        public IActionResult Status()
+        {
+            using (RsdbContext rsdbContext = new RsdbContext())
+            {
+                if (rsdbContext.Database.CanConnect())
+                {
+                    return Ok(true);
+                }
+                else
+                {
+                    return NoContent();
+                }
+            }
+        }
+    }
+}
